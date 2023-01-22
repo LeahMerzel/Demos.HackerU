10f11b3a6f7f9c0ac50f754f2000fd72f42e3ab1
shor-gad-github <shorgad@outlook.com>
2023-01-22 22:28:11 +0200

    Add project files.

---
+++ Demos.HackerU/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Demos.HackerU.Testing;
+
+namespace Demos.HackerU
+{
+    //--Lavel 2 : Class
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            //--01: NamespacesDemos and Classes
+            //Program.NamespacesDemos();
+            
+            //--02: Strings 
+            Program.Strings();
+
+
+
+            //--WAIT FOR USER PRESS ANY KEY--
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void Strings()
+        {
+            //----------------------------------------------------
+            //02 - Strings
+            //----------------------------------------------------
+            CommonTypesStrings.TestString();
+            //CommonTypesStrings.Lab1();
+            //CommonTypesStrings.Lab2("abc","def");
+            //-----------
+            //string res = CommonTypesStrings.Lab3("ABC", "DEF");
+            //Console.WriteLine(res);
+            //-----------
+            // bool isLoggedIn =  CommonTypesStrings.Lab4_Login("user123", "Pa$$w0rd");
+            // Console.WriteLine($"is Logged In : {isLoggedIn}");
+            //-----------------------------------------------------
+            // bool isLoggedIn = CommonTypesStrings.Lab5_Login("user", "12345");
+            // Console.WriteLine($"is Logged In : {isLoggedIn}");
+
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void NamespacesDemos()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Print Any Key to Exit");
+
+            //--Call Method A 2 times
+            //Use Full name
+            Demos.HackerU.MyClass.MethodA();
+
+            //Full Name Is not Required Because 
+            //MyClass is in the same Namspace
+            //as caller class (Program)
+            //Program and MyClass are in the same namspace
+            //So Full Name is Not Reuired
+            MyClass.MethodA();
+
+            //--Call Method B 
+            MyClass.MethodB();
+
+            //--Call Testing Full Name
+            Demos.HackerU.Testing.Test.TestA();
+
+            //--We put "using Demos.HackerU.Testing;" at the top file 
+            //--So Full Name Is Not Required
+            Test.TestA();
+
+            Test.TestA();
+            Test.MethodB();
+
+
+            Console.ReadKey();
+        }
+      
+    }
+}
