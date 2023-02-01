2c0810f5404581bfe8b50667c4d04143a51fbfdb
shor-gad-github <shorgad@outlook.com>
2023-02-01 20:50:38 +0200

    InnerLoop

--- Demos.HackerU.CommonTypes/ArraysAndLoops.cs
+++ Demos.HackerU.CommonTypes/ArraysAndLoops.cs
@@ -9,6 +9,10 @@ namespace Demos.HackerU.CommonTypes
 {
     public class ArraysAndLoops
     {
+
+     
+
+
         public static void Run()
         {
             ListIterations();
@@ -19,10 +23,15 @@ namespace Demos.HackerU.CommonTypes
             //--Object of type Array 
             List<int?> l = new List<int?>();
             l.Add(1);
-            l.Add(10);
+            l.Add(10);//1 10
+            l.Add(20);//1 10 20
+            l.Insert(1, 1000);//1 100 10 20
+            l.RemoveAt(2);//1 1000 20
+            l.Remove(1000);
+
             var l2 = new List<int>() { 10,20,30 };
             int founditem = l2.Find(n => n >10);
-            int foundindex= l2.FindIndex(n => n == 90);
+            int foundindex= l2.FindIndex(n => n == 30);
             if (foundindex>0)
             {
                 Console.WriteLine(foundindex);
@@ -33,14 +42,32 @@ namespace Demos.HackerU.CommonTypes
                 Console.WriteLine(numItem);
             }
 
+            l2.ForEach(a => Console.WriteLine(a));
 
-
-
+            List<int> subList = l2.FindAll(n => n > 10);
+            foreach (int item in subList)
+            {
+                Console.WriteLine(item);
+            }
 
         }
 
 
-
+        //Get From Users 2 Parameters
+        // - width
+        // - hight
+        // - char tav
+        //Print Rectangle on the console 
+        public static void DrawRectangle(int height,int width,char tav)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(tav);
+                }
+            }
+        }
 
 
     }
