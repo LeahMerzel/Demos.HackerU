ad727a6555477e522fbbd2238554b25dc3232d48
shor-gad-github <shorgad@outlook.com>
2023-02-08 21:34:28 +0200

    Object And Classes

--- Demos.HackerU.OOP/OOPDemos.cs
+++ Demos.HackerU.OOP/OOPDemos.cs
@@ -40,7 +40,20 @@ namespace Demos.HackerU.OOP
 
             //Array Of Objects
             Point[] points = { p1, p2, p3 };
+           
+            foreach (Point pointObj in points)
+            {
+                pointObj.Display();
+            }
+
             points[0].Reset();
+            points[1].Reset();
+            points[2].Reset();
+
+            foreach (Point pointObj in points)
+            {
+                pointObj.Display();
+            }
 
             Point[] points2 = { new Point{X=0,Y=1 }, 
                                 new Point { X = 10, Y = 10 } };
