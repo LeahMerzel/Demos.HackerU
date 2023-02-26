f6becf00f7d1770db36134ce4e935c94981091b0
shor-gad-github <shorgad@outlook.com>
2023-02-26 18:27:12 +0200

    Emloyee

---
+++ Demos.HackerU.OOP/Polimorphism/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.HackerU.OOP.Polimorphism
+{
+    public class Employee
+    {
+       public int BaseSalary { get; set; }
+
+      
+
+        public Employee(int baseSalary)
+        {
+            BaseSalary = baseSalary;
+        }
+
+
+    }
+}
